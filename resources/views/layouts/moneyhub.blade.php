f53f775c7da1270729e932ae65c8e19efb8440e9
knps01 <kingnapa.s@kkumail.com>
2023-09-23 03:13:09 +0700

    แก้บัค

--- resources/views/layouts/moneyhub.blade.php
+++ resources/views/layouts/moneyhub.blade.php
@@ -22,7 +22,7 @@
         </header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item"><a class="nav-link active" href="{{ route('moneyhub.home') }}">หน้าหลัก</a></li>
+                <li class="nav-item"><a class="nav-link active" href="{{ route('moneyhub.indexhome') }}">หน้าหลัก</a></li>
                 <li class="nav-item"><a class="nav-link" href="{{ route('moneyhub.noteincome', ['user_id' => auth()->user()->id])}}">รายรับ-รายจ่าย</a></li>
                 <li class="nav-item"><a class="nav-link" href="{{ route('moneyhub.summarize') }}">สรุป</a></li>
                 <li class="nav-item"><a class="nav-link" href="">ประวัติ</a></li>
