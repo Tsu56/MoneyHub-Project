2fc76fdcea192fe3d5d624ec907e4437b5d010ac
sorawitPHX <sorawit.ph@kkumail.com>
2023-10-04 18:39:14 +0700

    แก้บัค boostrap script ทับกัน

--- resources/views/layouts/moneyhub.blade.php
+++ resources/views/layouts/moneyhub.blade.php
@@ -10,7 +10,6 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <!-- @bootstrap -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-        <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
         <!-- @style.css -->
         <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
         <!-- @fonts.google -->
@@ -145,7 +144,7 @@
     </footer>
     <!-- ***** Footer End ***** -->
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script>
         if (auth()->user()->is_admin == 1) {
             document.getElementById('adminhome').hidden = false;
