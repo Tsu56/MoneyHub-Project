d2a4daa5a8c8bfe70a233a1e1ec02063a8ba5300
sorawitPHX <sorawit.ph@kkumail.com>
2023-10-05 12:40:00 +0700

    Revert "แก้ script ตรวจสอบ admin ใหม่ ตรง layout"
    This reverts commit 389e7cfecf1cdc10f1dad6aadcd1be945260ff4a.

--- resources/views/layouts/moneyhub.blade.php
+++ resources/views/layouts/moneyhub.blade.php
@@ -146,10 +146,9 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script>
-        @if (auth()->user()->is_admin == 1) {
+        if ({{auth()->user()->is_admin}} == 1) {
             document.getElementById('adminhome').hidden = false;
         }
-        @endif
     </script>
 </body>
 
