0e56b62aef383cebc6ff302adcf48d1fcf847c03
knps01 <kingnapa.s@kkumail.com>
2023-10-06 01:51:03 +0700

    เอาไอคอนออกแล้วใส่ที่อยู่แทน

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Enquiry;
 
 class ContactController extends Controller
 {
@@ -11,5 +11,16 @@ class ContactController extends Controller
     {
 
         return view('contact/contact');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'msg' => 'required|string',
+        ]);
+
+        return redirect()->route('moneyhub.contact')->with('success', 'ส่งข้อความสำเร็จ!');;
     }
 }
