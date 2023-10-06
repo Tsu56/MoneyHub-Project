811ad0afec6949945cc2333d211bef0fecfa4f48
sorawitPHX <sorawit.ph@kkumail.com>
2023-10-06 08:36:35 +0700

    ลบ menu

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -12,11 +12,11 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                {{-- <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link href="{{ route('moneyhub.indexhome') }}" :active="request()->routeIs('moneyhub.indexhome')">
                         {{ __('หน้าแรก') }}
                     </x-nav-link>
-                </div>
+                </div> --}}
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
